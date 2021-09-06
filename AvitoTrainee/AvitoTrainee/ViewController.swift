77c8c22743adf1412cee5e61d1e8b68ae83f9e9c
nlitvinov90 <72935116+nlitvinov90@users.noreply.github.com>
2021-09-06 19:47:06 +0300

    Add Cell configure

--- AvitoTrainee/AvitoTrainee/ViewController.swift
+++ AvitoTrainee/AvitoTrainee/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import PinLayout
 
 class ViewController: UIViewController{
 
@@ -18,6 +18,8 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Avito"
+        
         setupTableView()
         setupActivityIndicator()
         loadEmployees()
@@ -26,8 +28,9 @@ class ViewController: UIViewController{
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        tableView.frame = view.frame
-        activityIndicatorView.frame = CGRect(x: 140, y: 140, width: 40, height: 40)
+        tableView.pin.all()
+        activityIndicatorView.pin
+            .center()
     }
     
     private func setupActivityIndicator(){
@@ -63,10 +66,15 @@ class ViewController: UIViewController{
                 self?.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
-                print("Что то пошло не так, проверьте соединение с интернетом")
-                // вставь сюда алерт
+                self?.showAlert(message: "Что-то пошло не так, проверьте соединение с интернетом")
             }
         }
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
 
@@ -80,17 +88,85 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeCell") as? EmployeeCell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeCell", for: indexPath) as? EmployeeCell else { return .init() }
         
-        let employee = employees[indexPath.row]
+        cell.configure(with: employees[indexPath.row])
         
-        cell?.textLabel?.text = employee.name
-        
-        return cell ?? .init()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
 }
 
 
 final class EmployeeCell: UITableViewCell{
+    
+    private let nameLabel = UILabel()
+    private let phone_numberLabel = UILabel()
+    private let firstSkillLabel = UILabel()
+    private let secondSkillLabel = UILabel()
+    private let thirdSkillLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup(){
+        
+        [nameLabel, phone_numberLabel, firstSkillLabel, secondSkillLabel, thirdSkillLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.pin
+            .top(8)
+            .left(12)
+            .height(60)
+            .sizeToFit(.height)
+        
+        phone_numberLabel.pin
+            .below(of: nameLabel)
+            .left(12)
+            .height(40)
+            .sizeToFit(.height)
+        
+        firstSkillLabel.pin
+            .top()
+            .right(12)
+            .height(40)
+            .sizeToFit(.height)
+        
+        secondSkillLabel.pin
+            .below(of: firstSkillLabel)
+            .right(12)
+            .height(40)
+            .sizeToFit(.height)
+        
+        thirdSkillLabel.pin
+            .below(of: secondSkillLabel)
+            .right(12)
+            .height(40)
+            .sizeToFit(.height)
+        
+    }
+    
+    func configure(with model: Employee){
+        nameLabel.text = model.name
+        phone_numberLabel.text = "Phone: \(model.phone_number)"
+        firstSkillLabel.text = model.skills[0]
+        secondSkillLabel.text = model.skills[1]
+        thirdSkillLabel.text = model.skills[2]
+    }
     
 }
